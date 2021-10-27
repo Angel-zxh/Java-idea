9359c115384e1e8b03b8a6e2eed0a246f942057c
章欣辉 <90779010+zhangxinhui428@users.noreply.github.com>
2021-10-27 18:18:56 +0800

    Delete src/Chap9_6 directory

--- src/Chap9_6/Icat.java
+++
@@ -1,6 +0,0 @@
-package Chap9_6;
-
-public interface Icat {
-    public void catLooks();
-    public void zhuoshu();
-}
--- src/Chap9_6/Idog.java
+++
@@ -1,6 +0,0 @@
-package Chap9_6;
-
-public interface Idog {
-    public void dogLooks();
-    public void goujiao();
-}
